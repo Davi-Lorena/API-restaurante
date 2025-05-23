227b84c5fd647941102853d13c9347b166070b8b
Davi Lorena <davilorena2016@gmail.com>
2025-05-23 09:32:10 -0300

    closing tables-sessions

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -42,6 +42,28 @@ try {
 }
     }
 
+    async update(request: Request, response: Response, next: NextFunction) {
+        try {
+            const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value), { message: "id must be a number"}).parse(request.params.id)
+
+const session = await knex<TablesSessionsRepository>("tables_sessions").where({ id }).first()
+
+if (!session) {
+    throw new AppError("Session not found")
+}
+
+if(session.closed_at) {
+     throw new AppError("This session table is already close")
+}
+
+await knex<TablesSessionsRepository>("tables_sessions").update({ closed_at: knex.fn.now() }).where({ id })
+
+return response.json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
 }
 
 export { TablesSessionsController }
