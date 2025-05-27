678e07bab1296c31b2513411b2e3b04529fe13db
Davi Lorena <davilorena2016@gmail.com>
2025-05-27 09:34:44 -0300

    listing orders

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -45,6 +45,19 @@ await knex<OrderRepository>("orders").insert({
     }
 }
 
+async index(request: Request, response: Response, next: NextFunction) {
+    try {
+        const { table_sessions_id } = request.params
+
+        const order = await knex("orders").select("orders.id", "orders.table_sessions_id", "orders.product_id", "products.name","orders.price", "orders.quantity", knex.raw("(orders.price * orders.quantity) AS total"), "orders.created_at", "orders.updated_at").join("products", "products.id", "orders.product_id"
+        ).where({ table_sessions_id }).orderBy("orders.created_at", "desc")
+
+        return response.json(order)
+    } catch (error) {
+        next(error)
+    }
+}
+
 }
 
 export { OrdersController }
